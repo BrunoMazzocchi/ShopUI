875b6a3b7106cb703f85990a467cc51aadef2706
Bruno Mazzocchi <bemazzocchi@gmail.com>
2023-03-16 14:24:46 -0600

    Home page and its components

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+import '../../../models/product.dart';
+import 'categories.dart';
+import 'item_card.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: kDefaultPadding,
+          ),
+          child: Text(
+            "Women",
+            style: Theme.of(context)
+                .textTheme
+                .headlineSmall
+                ?.copyWith(fontWeight: FontWeight.bold),
+          ),
+        ),
+        const Categories(),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            child: GridView.builder(
+              itemCount: products.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.75,
+                mainAxisSpacing: kDefaultPadding,
+                crossAxisSpacing: kDefaultPadding,
+              ),
+              itemBuilder: (context, index) => ItemCard(product: products[index], onPress: (){}),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
